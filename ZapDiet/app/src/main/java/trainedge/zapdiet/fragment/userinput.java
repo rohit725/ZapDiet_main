30e2ae35d52d5f3ad6f487be93553de5826d89de
zaid bin kamil <xaidmetamorphos@gmail.com>
2017-04-18 15:54:53 +0530

    new commit

--- ZapDiet/app/src/main/java/trainedge/zapdiet/fragment/userinput.java
+++ ZapDiet/app/src/main/java/trainedge/zapdiet/fragment/userinput.java
@@ -14,6 +14,7 @@ import trainedge.zapdiet.R;
 public class userinput extends Fragment {
 
     String[] spinlist =new String[]{"Kg","Pound"};
+    String[] genderlist =new String[] {"Male","Female"};
 
     public userinput() {
         // Required empty public constructor
@@ -25,9 +26,13 @@ public class userinput extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_userinput, container, false);
         Spinner spin = (Spinner) view.findViewById(R.id.spinner);
+        Spinner gend = (Spinner) view.findViewById(R.id.gender);
         ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, spinlist);
+        ArrayAdapter<String> arrayadapter=new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, genderlist);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        arrayadapter .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spin.setAdapter(arrayAdapter);
+        gend.setAdapter(arrayadapter);
         return view;
     }
 
